619817242fcbf6c9a2917ac097184cc859371fda
Greg Kluska <kluskagreg@gmail.com>
2020-07-26 23:47:55 +0100

    Add model and a test file

---
+++ app/src/test/java/com/gregkluska/unittesting/models/NoteTest.java
@@ -0,0 +1,34 @@
+package com.gregkluska.unittesting.models;
+
+import org.junit.jupiter.api.Test;
+
+public class NoteTest {
+
+    /*
+        Compare two equal Notes
+     */
+    @Test
+    void isNotesEqual_identicalProperties() throws Exception {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    /*
+        Compare notes with 2 different ids
+     */
+
+    /*
+        Compare two notes with different timestamp
+     */
+
+    /*
+        Compare two notes with different tittles
+     */
+
+    /*
+        Compare two notes with different content
+     */
+}
