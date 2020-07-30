6338487259056fc62219c57f9e52654f74d29461
Greg Kluska <kluskagreg@gmail.com>
2020-07-30 19:37:56 +0100

    Add Room

--- app/src/main/java/com/gregkluska/unittesting/di/AppModule.java
+++ app/src/main/java/com/gregkluska/unittesting/di/AppModule.java
@@ -1,7 +1,35 @@
 package com.gregkluska.unittesting.di;
 
+import android.app.Application;
+
+import androidx.room.Room;
+
+import com.gregkluska.unittesting.persistence.NoteDao;
+import com.gregkluska.unittesting.persistence.NoteDatabase;
+
+import javax.inject.Singleton;
+
 import dagger.Module;
+import dagger.Provides;
+
+import static com.gregkluska.unittesting.persistence.NoteDatabase.DATABASE_NAME;
 
 @Module
 public class AppModule {
+
+    @Singleton
+    @Provides
+    static NoteDatabase provideNoteDatabase(Application application) {
+        return Room.databaseBuilder(
+                application,
+                NoteDatabase.class,
+                DATABASE_NAME
+        ).build();
+    }
+
+    @Singleton
+    @Provides
+    static NoteDao provideNoteDao(NoteDatabase noteDatabase) {
+        return noteDatabase.getNoteDao();
+    }
 }
---
+++ app/src/main/java/com/gregkluska/unittesting/persistence/NoteDatabase.java
@@ -0,0 +1,14 @@
+package com.gregkluska.unittesting.persistence;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+import com.gregkluska.unittesting.models.Note;
+
+@Database(entities = {Note.class}, version = 1)
+public abstract class NoteDatabase extends RoomDatabase {
+
+    public static final String DATABASE_NAME = "notes_db";
+
+    public abstract NoteDao getNoteDao();
+}
